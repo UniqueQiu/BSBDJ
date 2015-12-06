b9c13c3967db348a384fb9800f8afadde1fbcd10
chuck <chuck@sina.com>
2015-12-06 16:15:52 +0800

    初始化项目

---
+++ BSBDJ_2/BSBDJ_2/Classes/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSBDJ_2
+//
+//  Created by Chuck on 12/4/15.
+//  Copyright © 2015 Chuck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
